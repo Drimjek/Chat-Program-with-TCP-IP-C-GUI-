20cbd569db73217963507ad9f7996746597b7646
Louis Bima Susanto <115883367+LouisBimaSusanto@users.noreply.github.com>
2025-09-24 22:55:00 +0700

    Add files via upload

---
+++ ClientApp/ChatMessage.cs
@@ -0,0 +1,8 @@
+﻿public class ChatMessage
+{
+    public string type { get; set; }   // "msg"|"join"|"leave"|"pm"|"sys"
+    public string from { get; set; }
+    public string to { get; set; }     // for pm, else null/empty
+    public string text { get; set; }
+    public long ts { get; set; }       // unix epoch seconds
+}
